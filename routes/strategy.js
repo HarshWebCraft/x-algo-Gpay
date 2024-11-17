00303dfec70477d57d3d974cdea43b01e776fe19
HarshTechCraft <harshdvadhavana26@gmail.com>
2024-11-17 19:03:47 +0530

    stra

--- routes/strategy.js
+++ routes/strategy.js
@@ -5,8 +5,8 @@ const { placeOrder } = require("../paper trade/placeOrder.js");
 const WebSocket = require("ws");
 
 // Define user input
-const candleTime = "18:25";
-const timeFrame = "30m";
+const candleTime = "19:08";
+const timeFrame = "1m";
 const symbol = "BTCUSD";
 const quantity = 10;
 const stop_loss_point = 10;
