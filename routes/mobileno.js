30be6972bfb3e2052b1702b651a4021a6f5c72f7
HarshWebCraft <harshwebdev26@gmail.com>
2024-12-07 22:57:47 +0530

    test 3 test 4

--- routes/mobileno.js
+++ routes/mobileno.js
@@ -1,17 +1,15 @@
-const User = require('../models/users')
+const User = require("../models/users");
 
-const a = async(req,res) =>{
-    try{
-        const user = await User.findOne({Email:req.body.userEmail})
-        if(user.MobileNo == ""){
-            return res.json({mobileNumber:true})
-        }
-        else{
-            return res.json({mobileNumber:false})
-        }
+const a = async (req, res) => {
+  try {
+    const user = await User.findOne({ Email: req.body.userEmail });
+    if (user.MobileNo) {
+      return res.json({ mobileNumber: true });
+    } else {
+      return res.json({ mobileNumber: false });
     }
-    catch(e){
-        console.log(e)
-    }
-}
-module.exports=a
+  } catch (e) {
+    console.log(e);
+  }
+};
+module.exports = a;
